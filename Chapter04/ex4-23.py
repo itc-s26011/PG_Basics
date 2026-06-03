dc7335386d6f75402c5e94554e184b60e7a063c0
itc-s26011 <s26011@std.it-college.ac.jp>
2026-06-03 07:59:00 +0000

    第４章例題２３まで

---
+++ Chapter04/ex4-23.py
@@ -0,0 +1,11 @@
+def add(x, y):
+    """
+    ２つの引数の和を返す関数add
+    引数その１：int x
+    引数その２：int y 
+    Returns x + y.
+    :param x: int.
+    :param y: int.
+    :return: int sum of x and y.
+    """
+    return x + y
